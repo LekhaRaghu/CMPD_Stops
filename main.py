8914b0446913c2b98c6ca87b7376f7a4cceaeb35
Yaxin Zhao <zhaoyaxin.66666@gmail.com>
2024-04-08 19:42:25 -0400

    add box plot
    update

--- main.py
+++ main.py
@@ -11,3 +11,12 @@ def load_data(csv):
 stops = pd.read_csv("data/Officer_Traffic_Stops.csv")
 
 st.dataframe(stops)
+
+## Box plot
+import altair as alt
+alt.Chart(stops).mark_boxplot().encode(
+    x = alt.X('Was_a_Search_Conducted'),
+    y = alt.Y('Driver_Age')
+).properties(
+    width = 500,
+    title = 'Boxplot between Search Conducted vs Driver Age')
